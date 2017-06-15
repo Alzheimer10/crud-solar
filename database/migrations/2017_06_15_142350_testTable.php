00c6d39ac20ebf845b1fdb7169b16112c943f1b0
Alzhiemer10 <carlosanselmi2@gmail.com>
2017-06-15 14:39:52 -0400

    new proyect

---
+++ database/migrations/2017_06_15_142350_testTable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testTable', function (Blueprint $table) {
+            $table->string('nombre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testTable');
+    }
+}
